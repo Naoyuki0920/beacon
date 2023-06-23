a6de962bc90782498b07d150c9184e6c738c4aa9
Naoyuki0920 <naoyuki.effort110920@gmail.com>
2023-06-23 16:40:00 +0900

    BLE RSSI

--- device_connect.py
+++ device_connect.py
@@ -1,38 +1,13 @@
-from bluepy.btle import Peripheral, DefaultDelegate
+import bluepy
 
-# ビーコンのUUID
+scanner = bluepy.btle.Scanner(0)
+device = scanner.scan(5)  #5秒間スキャン
 
-# ビーコンのアドレス
-beacon_address = "XX:XX:XX:XX:XX:XX"
-
-# 距離計算のための定数
-RSSI_1m = -60  # ビーコンから1m離れた場合のRSSI値
-
-# ビーコンからの距離を計算する関数
-def calculate_distance(rssi):
-    ratio = rssi / RSSI_1m
-    if ratio < 1.0:
-        return pow(ratio, 10)
-    else:
-        return (0.89976) * pow(ratio, 7.7095) + 0.111
-
-# ビーコンとの通信用のデリゲートクラス
-class BeaconDelegate(DefaultDelegate):
-    def __init__(self):
-        DefaultDelegate.__init__(self)
-
-    def handleNotification(self, cHandle, data):
-        # ビーコンからの通知を受け取った場合の処理
-        rssi = int.from_bytes(data, byteorder='little', signed=True)
-        distance = calculate_distance(rssi)
-        print("Distance:", distance, "m")
-
-# ビーコンとの接続および通信開始
-beacon = Peripheral(beacon_address)
-beacon.withDelegate(BeaconDelegate())
-beacon.writeCharacteristic(0x12, b"\x01\x00")  # ビーコンの通知を有効化
-
-# 通信ループを開始
-while True:
-    if beacon.waitForNotifications(1.0):
-        continue
+for device in device:
+    print('======================================================')
+    print('address : %s' % device.addr)
+    print('addrType: %s' % device.addrType)
+    print('RSSI    : %s' % device.rssi)
+    print('Adv data:')
+    for (adtype, desc, value) in device.getScanData():
+        print(' (%3s) %s : %s ' % (adtype, desc, value))
