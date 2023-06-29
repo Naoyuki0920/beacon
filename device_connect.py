f62f504818977f193fb804871d9fa9b2b3699876
Naoyuki0920 <naoyuki.effort110920@gmail.com>
2023-06-29 13:59:35 +0900

    update distance

--- device_connect.py
+++ device_connect.py
@@ -4,10 +4,12 @@ scanner = bluepy.btle.Scanner(0)
 device = scanner.scan(5)  #5秒間スキャン
 
 for device in device:
-    print('======================================================')
-    print('address : %s' % device.addr)
-    print('addrType: %s' % device.addrType)
-    print('RSSI    : %s' % device.rssi)
-    print('Adv data:')
-    for (adtype, desc, value) in device.getScanData():
-        print(' (%3s) %s : %s ' % (adtype, desc, value))
+    if device.addr == "3c:01:ef:33:d8:b8":
+        distance = 10^( -59 - device.rssi)/(10 * 2)
+        print('address : %s' % device.addr)
+        print('addrType: %s' % device.addrType)
+        print('RSSI    : %s' % device.rssi)
+        print('range   : %s' % distance)
+        print('Adv data:')
+        for (adtype, desc, value) in device.getScanData():
+            print(' (%3s) %s : %s ' % (adtype, desc, value))
