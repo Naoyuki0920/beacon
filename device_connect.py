7cf8ece52d3f20b5828d1afa5bb1e8cbfa1fe780
Naoyuki0920 <naoyuki.effort110920@gmail.com>
2023-06-29 14:02:16 +0900

    update distance

--- device_connect.py
+++ device_connect.py
@@ -5,7 +5,7 @@ device = scanner.scan(5)  #5秒間スキャン
 
 for device in device:
     if device.addr == "3c:01:ef:33:d8:b8":
-        distance = 10^( -59 - device.rssi)/(10 * 2)
+        distance = 10 ** (-59 - device.rssi)/(10 * 2)
         print('address : %s' % device.addr)
         print('addrType: %s' % device.addrType)
         print('RSSI    : %s' % device.rssi)
