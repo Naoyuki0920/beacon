f3a74d608d36f665b3f9d574f57118fc233a88c7
Naoyuki0920 <naoyuki.effort110920@gmail.com>
2023-07-04 13:24:19 +0900

    glbfile send

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,11 @@
-from flask import Flask 
+from flask import Flask, send_file
 
 app = Flask( __name__ ) 
 
-@app.route('/') 
-def hello_world():
-    return 'Hello World'; 
+@app.route('/', methods=['GET']) 
+def send_ARData():
+    file_path = "../NeilArmstrong.glb"
+    return send_file(file_path, as_attachment=True)
 
 if __name__ == '__main__': 
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
