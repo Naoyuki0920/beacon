3fb1417591398b95943521272467176cd8db8bdf
Naoyuki0920 <naoyuki.effort110920@gmail.com>
2023-07-13 13:02:01 +0900

    AR.js導入

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, send_file, render_template
+from flask_cors import CORS
+
+app = Flask( __name__ ) 
+CORS(app)
+
+@app.route('/', methods=['GET']) 
+def index():
+    return render_template("index.html")
+# def send_ARData():
+#     file_path = "NeilArmstrong.glb"
+#     return send_file(file_path, as_attachment=True)
+
+if __name__ == '__main__': 
+    app.run(host='0.0.0.0', port=5000)
