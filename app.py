d26a8fcff1cd4f3f0d4dcde0816ead33acbe5f98
Naoyuki0920 <naoyuki.effort110920@gmail.com>
2023-07-14 12:06:44 +0900

    元に戻す

--- app.py
+++ app.py
@@ -5,16 +5,11 @@ app = Flask( __name__ )
 CORS(app)
 
 @app.route('/', methods=['GET']) 
-def index():
-    return render_template("index.html")
-# def send_ARData():
-#     file_path = "NeilArmstrong.glb"
-#     return send_file(file_path, as_attachment=True)
+# def index():
+#     return render_template("index.html")
+def send_ARData():
+    file_path = "NeilArmstrong.glb"
+    return send_file(file_path, as_attachment=True)
 
-if __name__ == '__main__': 
-    import ssl
-    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-    ssl_context.load_cert_chain(
-        'fullchain.pem', 'privkey.pem'
-    )
-    app.run(host='0.0.0.0', port=5000, ssl_context=ssl_context)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
