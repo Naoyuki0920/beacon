bd41f77f8ea18b43df2385fcd9657c1c37e686e7
Naoyuki0920 <naoyuki.effort110920@gmail.com>
2023-07-25 13:02:40 +0900

    いったんもとに戻す

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask, send_file, render_template, send_from_directory
+from flask import Flask, send_file, render_template
 from flask_cors import CORS
 
 app = Flask( __name__ ) 
 CORS(app)
 
-@app.route('/<path:filename>', methods=['GET']) 
+@app.route('/', methods=['GET']) 
 # def index():
 #     return render_template("index.html")
-def send_ARData(filename):
-    return send_from_directory("/static", filename, as_attachment=True, mimetype="model/gltf-binary")
+def send_ARData():
+    file_path = "NeilArmstrong.glb"
+    return send_file(file_path, as_attachment=True, mimetype="model/gltf-binary")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
