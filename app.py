47a60bb8dace23f7d1a0d4cc50a90a990d96041e
Naoyuki0920 <naoyuki.effort110920@gmail.com>
2023-07-25 13:24:42 +0900

    mimetype変更

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from flask_cors import CORS
 app = Flask( __name__ ) 
 CORS(app)
 
-@app.route('/', methods=['GET']) 
-# def index():
-#     return render_template("index.html")
-def send_ARData():
-    file_path = "NeilArmstrong.glb"
-    return send_file(file_path, as_attachment=True, mimetype="application/json")
+@app.route('/get_glb', methods=['GET'])
+def get_glb():
+    # glbファイルのパスを指定してバイナリデータとして読み込む
+    glb_file_path = 'NeilArmstrong.glb'
+    return send_file(glb_file_path, as_attachment=True, mimetype='model/gltf-binary')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
