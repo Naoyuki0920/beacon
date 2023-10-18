00b97b060a8a2cf095d345fd01f0ac962f359c4d
Naoyuki0920 <naoyuki.effort110920@gmail.com>
2023-10-18 11:12:50 +0900

    add requests

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from flask import Flask, send_file
 from flask_cors import CORS
 import requests
+import os
 
-app = Flask( __name__ ) 
+app = Flask(__name__)
 CORS(app)
+
 
 @app.route('/get_glb', methods=['GET'])
 def get_glb():
-    requests.get('http://192.168.75.10:5000/confirm_glb/1')
-    glb_file_path = 'Astronaut.glb'
+    api_url = 'http://192.168.75.10:5000/confirm_glb/1'
+    response = requests.get(api_url)
+
+    save_directory = "/"
+    os.makedirs(save_directory, exist_ok=True)
+    file_path = os.path.join(save_directory, '1.glb')
+    with open(file_path, 'wb') as file:
+        file.write(response.content)
+
+    glb_file_path = '1.glb'
     return send_file(glb_file_path, as_attachment=True, mimetype='model/gltf-binary')
 
 
