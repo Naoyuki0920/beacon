40062e77e836d122cd12dcd80e1b299d6108d5ea
Naoyuki0920 <naoyuki.effort110920@gmail.com>
2023-10-18 10:58:46 +0900

    add requests

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, send_file, render_template
+from flask import Flask, send_file
 from flask_cors import CORS
+import requests
 
 app = Flask( __name__ ) 
 CORS(app)
 
 @app.route('/get_glb', methods=['GET'])
 def get_glb():
-    # glbファイルのパスを指定してバイナリデータとして読み込む
-    # glb_file_path = 'NeilArmstrong.glb'
+    requests.get('http://192.168.75.10:5000/confirm_glb/1')
     glb_file_path = 'Astronaut.glb'
     return send_file(glb_file_path, as_attachment=True, mimetype='model/gltf-binary')
 
