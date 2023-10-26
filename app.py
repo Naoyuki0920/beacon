44bee5bb2e1b3519198c5f8266c5d273c860faba
Naoyuki0920 <naoyuki.effort110920@gmail.com>
2023-10-26 15:47:27 +0900

    参考

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import io
+import zipfile
 from flask import Flask, send_file
 from flask_cors import CORS
 import requests
@@ -9,17 +11,21 @@ CORS(app)
 
 @app.route('/get_glb', methods=['GET'])
 def get_glb():
+    # 子機ごとにURLのさいごの数字を変更
     api_url = 'http://192.168.75.10:5000/confirm_glb/1'
     response = requests.get(api_url)
 
     save_directory = "./"
-    os.makedirs(save_directory, exist_ok=True)
-    file_path = os.path.join(save_directory, '1.glb')
-    with open(file_path, 'wb') as file:
-        file.write(response.content)
+    with zipfile.ZipFile(io.BytesIO(response.content), 'r') as zip_ref:
+        # ファイルを保存するディレクトリが存在しない場合は作成
+        if not os.path.exists(save_directory):
+            os.makedirs(save_directory)
 
-    glb_file_path = '1.glb'
-    return send_file(glb_file_path, as_attachment=True, mimetype='model/gltf-binary')
+        # ZIPファイル内のファイルを展開して保存
+        zip_ref.extractall(save_directory)
+
+    # glb_file_path = '1.glb'
+    # return send_file(glb_file_path, as_attachment=True, mimetype='model/gltf-binary')
 
 
 if __name__ == '__main__':
