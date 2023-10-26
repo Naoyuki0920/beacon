d4de744fa83085222dcfb15565b4e3d870b5bb0b
Naoyuki0920 <naoyuki.effort110920@gmail.com>
2023-10-26 16:30:46 +0900

    参考

--- app.py
+++ app.py
@@ -11,6 +11,12 @@ CORS(app)
 
 @app.route('/get_glb', methods=['GET'])
 def get_glb():
+    # ディレクトリ内のファイルを取得
+    files = os.listdir("./static")
+    for file in files:
+        file_path = os.path.join("./static", file)
+        os.remove(file_path)
+    
     # 子機ごとにURLのさいごの数字を変更
     api_url = 'http://192.168.75.10:5000/confirm_glb/1'
     response = requests.get(api_url)
@@ -23,6 +29,17 @@ def get_glb():
 
         # ZIPファイル内のファイルを展開して保存
         zip_ref.extractall(save_directory)
+    
+    files = os.listdir(save_directory)
+    zip_filename = 'all_files.zip'
+    # ZIPファイルを作成
+    with zipfile.ZipFile(zip_filename, 'w') as zipf:
+        for file in files:
+            # ファイルをZIPに追加
+            file_path = os.path.join(save_directory, file)
+            zipf.write(file_path, os.path.basename(file_path))
+    # ZIPファイルをクライアントに送信
+    return send_file(zip_filename, as_attachment=True)
 
     # glb_file_path = '1.glb'
     # return send_file(glb_file_path, as_attachment=True, mimetype='model/gltf-binary')
