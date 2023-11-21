b13c412a4a6b5e6808e2e4ea7a46939b043a3603
Naoyuki0920 <naoyuki.effort110920@gmail.com>
2023-11-21 14:30:29 +0900

    CORS追加

--- app.py
+++ app.py
@@ -1,31 +1,33 @@
-import os
 import io
 import zipfile
-import requests
 from flask import Flask, send_file
+from flask_cors import CORS
+import requests
+import os
 
 app = Flask(__name__)
+CORS(app)
 
+
+@app.route('/get_glb', methods=['GET'])
 def get_glb():
     # 子機ごとにURLのさいごの数字を変更
-    api_url = 'http://192.168.75.10:5000/confirm_glb/1'
-    zip_filename = 'all_files.zip'
+    api_url = 'http://192.168.186.10:5000/confirm_glb/1'
 
     try:
         response = requests.get(api_url)
 
         if response.status_code == 200:
+            zip_filename = 'all_files.zip'
             with open(zip_filename, 'wb') as zip_file:
                 zip_file.write(response.content)
 
         elif response.status_code == 404:
             print('APIが見つかりませんでした。')
-            print(f'Response Content: {response.text}')
 
         # その他のステータスコードの場合
         else:
             print(f'APIにアクセスできませんでした。ステータスコード: {response.status_code}')
-            print(f'Response Content: {response.text}')
 
     except requests.exceptions.RequestException as e:
         # リクエストに関連するエラーが発生した場合
@@ -38,5 +40,5 @@ def get_glb():
     # ZIPファイルをクライアントに送信
     return send_file(zip_filename, as_attachment=True)
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
