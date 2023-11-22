3461e70e4adde7ca34f2c76a0db71763e6eea5ec
Naoyuki0920 <naoyuki.effort110920@gmail.com>
2023-11-22 10:39:25 +0900

    スレッドか

--- app.py
+++ app.py
@@ -42,4 +42,4 @@ def get_glb():
     return send_file(zip_filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, threaded=True)
